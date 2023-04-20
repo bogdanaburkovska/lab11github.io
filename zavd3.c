5f9db14a355468abefc87f810a23c1a37c9ceee3
Богдана Бурковська <128155212+bogdanaburkovska@users.noreply.github.com>
2023-04-20 10:53:14 +0300

    site

--- zavd3.c
+++ zavd3.c
@@ -2,11 +2,10 @@
 #include <math.h>
 
 int main() {
-    double x = 0.0;
-    double y = pow(x, 3) - 4 * pow(x, 2) - 5 * x + 9 + cos(x);
-  //значення функції при x = 0 дорівнює 10
-
-    printf("При x = %lf, y = %lf\n", x, y);
-
+float x, y;
+printf("Введіть x: ");
+scanf("%f", &x);
+y = pow(x, 3) - 4 * pow(x, 2) - 5 * x + 9 + cos(x);
+    printf("При x = %.2lf, y = %.2lf\n", x, y);
     return 0;
 }
